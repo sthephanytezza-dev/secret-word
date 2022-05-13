69bc649ea0863016e729fa00713a927a13dbd097
sthephanytezza-dev <sthephany.tezza@rethink.dev>
2022-05-13 15:01:41 -0300

    alternando os estágios do jogo

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import { wordsList } from "./data/words";
 // Components
 import StarScreen from "./components/StartScreen";
 import Game from "./components/Game";
+import GameOver from "./components/GameOver";
 
 const stages = [
   { id: 1, name: "start" },
@@ -21,13 +22,26 @@ function App() {
   const [gameStage, setGameStage] = useState(stages[0].name);
   const [words] = useState(wordsList);
 
-  console.log(words);
+  //starts the secret word game
+  const startGame = () => {
+    setGameStage(stages[1].name);
+  };
+
+  //process the letter input
+  const verifyLetter = () => {
+    setGameStage(stages[2].name);
+  };
+
+  //restarts the game
+  const retry = () => {
+    setGameStage(stages[0].name);
+  };
 
   return (
     <div className="App">
-      {gameStage === "start" && <StarScreen />}
-      {gameStage === "game" && <Game />}
-      {gameStage === "end" && <StarScreen />}
+      {gameStage === "start" && <StarScreen startGame={startGame} />}
+      {gameStage === "game" && <Game verifyLetter={verifyLetter} />}
+      {gameStage === "end" && <GameOver retry={retry} />}
     </div>
   );
 }
