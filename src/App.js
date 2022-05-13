0a5f54b1b0fe0bf74c62b0c9b4678b1f11de3b4a
sthephanytezza-dev <sthephany.tezza@rethink.dev>
2022-05-13 14:48:33 -0300

    criando componentes do projeto

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
+// CSS
 import "./App.css";
 
+// React
+import { useCallback, useEffect, useState } from "react";
+
+// Data
+import { wordsList } from "./data/words";
+
+// Components
 import StarScreen from "./components/StartScreen";
+import Game from "./components/Game";
+
+const stages = [
+  { id: 1, name: "start" },
+  { id: 2, name: "game" },
+  { id: 3, name: "end" },
+];
 
 function App() {
+  const [gameStage, setGameStage] = useState(stages[0].name);
+  const [words] = useState(wordsList);
+
+  console.log(words);
+
   return (
     <div className="App">
-      <StarScreen />
+      {gameStage === "start" && <StarScreen />}
+      {gameStage === "game" && <Game />}
+      {gameStage === "end" && <StarScreen />}
     </div>
   );
 }
