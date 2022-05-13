6c8a5645dcb85eeb3539cc6a5b2642e739944660
sthephanytezza-dev <sthephany.tezza@rethink.dev>
2022-05-13 16:09:08 -0300

    evento de inserção de letra

--- src/App.js
+++ src/App.js
@@ -65,8 +65,8 @@ function App() {
   };
 
   //process the letter input
-  const verifyLetter = () => {
-    setGameStage(stages[2].name);
+  const verifyLetter = (letter) => {
+    console.log(letter);
   };
 
   //restarts the game
